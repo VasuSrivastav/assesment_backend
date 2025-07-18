bc6f6b8f79db338ed07f5bd63ceb720b7cf334a6
VasuSri <vasusrivastav1@gmail.com>
2025-07-18 21:52:55 +0530

    deployment done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 import express, { json } from "express";
 import env from "dotenv";
-import cors from 'cors';
-import router from './routes.js'
+import cors from "cors";
+import router from "./routes.js";
 
 env.config();
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 
-app.use(cors({
-  origin: ['localhost:5173','http://localhost:5173'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "localhost:5173",
+      "http://localhost:5173",
+      "https://assesment-frontend-hazel-one.vercel.app/",
+      "https://assesment-frontend-hazel-one.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 let PORT = process.env.PORT || 3000;
 // let PORT = 4000;
-app.use('/api',router)
+app.use("/api", router);
 
 app.get("/", (req, res) => {
-    res.json("server is working");
+  res.json("server is working");
 });
 
-
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, "localhost", () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
